7c27dc3c3ef45f69695f05d4bb8b095315407e96
wuhaojie <w19961009@126.com>
2016-10-20 21:45:34 +0800

    init.

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    moduleId: module.id
+})
+
+export class AppComponent {
+    title = "Test";
+
+
+    login(): void{
+        
+    }
+
+
+}
